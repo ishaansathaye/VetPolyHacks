b2fa9f5d3a66a2eabc895dbfdc46876c5c845202
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-03-05 17:47:47 -0800

    wrote component that finds interests in common among user and the users in the database

--- interests.py
+++ interests.py
@@ -20,7 +20,7 @@ def initialize_interest_dict(veteranObjDict):
                 interests_database[interest] = [veteran_id]
     return interests_database
 
-def find_interests_connections(user_id, interests_database):
+def make_common_interests_dict(user_id, interests_database):
     user_interest_dict = {}
     for interest in veterans[user_id].interests:
         for user in interests_database[interest]:
@@ -28,8 +28,19 @@ def find_interests_connections(user_id, interests_database):
                 if user not in user_interest_dict:
                     user_interest_dict[user] = 1
                 else:
-                    user_interest_dict[]
-
-interest_dict = initialize_interest_dict(veterans)
-print([key for key in interest_dict])
+                    user_interest_dict[user] += 1
+    return determine_common_users(user_interest_dict)
     
+def determine_common_users(interestDict):
+    common_users = []
+    for user in interestDict:
+        if interestDict[user] >= 4:
+            common_users.append(user)
+    return common_users
+
+current_user = 'JB1994'
+interests_database = initialize_interest_dict(veterans)
+final_interests = make_common_interests_dict(current_user, interests_database)
+for user_id in final_interests:
+    final_list = ", ".join(list(set(veterans[current_user].interests) & set(veterans[user_id].interests))).lower()
+    print("You and", veterans[user_id].first, veterans[user_id].last, "have interests like", final_list, "or more in common.")
